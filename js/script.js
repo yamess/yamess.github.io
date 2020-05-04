901d7c745e9553f9fb41df67b228fa8bba5d1799
yamess <yameogo.boukari@gmail.com>
2020-05-03 22:06:24 -0400

    Sidebar added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-function hoverfunc() {
-    $(document).getElementsByClassName(".fab").hover(
-
-    )
-}
+$(document).getElementsByClassName('.wrapper .sidebar ul li').hover(function() {
+    $(".wrapper .sidebar ul li a .fas").toggleClass('.wrapper .sidebar ul li a .fas:hover')
+})
