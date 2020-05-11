56593bbe5159585edafc0bb665b36190e5426d22
yamess <yameogo.boukari@gmail.com>
2020-05-10 20:48:34 -0400

    Dark and Light theme added

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -10,6 +10,21 @@ function setActiveStyle(color) {
         }
     }
 }
+
+// Body skin
+const bodySkin=document.querySelectorAll('.body-skin'),
+    totalBodySkin=bodySkin.length;
+
+    for (let i=0; i<totalBodySkin; i++){
+        bodySkin[i].addEventListener('change', function (){
+            if (this.value === 'dark'){
+                document.body.className='dark';
+            } else {
+                document.body.className='';
+            }
+        })
+    }
+
 document.querySelector('.toggle-style-switcher').addEventListener('click', ()=>{
     document.querySelector('.style-switcher').classList.toggle('open');
 })
