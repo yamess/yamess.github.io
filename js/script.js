6f19662b7e5de41b98973e7f0ba578d425ae40ab
yamess <yameogo.boukari@gmail.com>
2020-05-11 21:42:49 -0400

    Adding command to change section with sidebar button

--- js/script.js
+++ js/script.js
@@ -81,3 +81,30 @@
             toggleLightbox();
         }
     })
+
+// Aside Navbar
+
+    const nav=document.querySelector(".nav"),
+        navList=nav.querySelectorAll("li"),
+        totalNavList=navList.length,
+        allSection=document.querySelectorAll(".section"),
+        totalSection=allSection.length;
+
+    for (let i=0; i<totalNavList; i++){
+        const a=navList[i].querySelector("a");
+        a.addEventListener("click", function () {
+            for (let j=0; j<totalNavList; j++){
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active");
+            showSection(this);
+        })
+    }
+
+    function showSection(element) {
+       for (let i=0;i<totalSection; i++){
+           allSection[i].classList.remove("active")
+       }
+        const target=element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active");
+    }
